876a177b1e78d37edb61ac67435bb58d476d8477
formoment <bitxlab@outlook.com>
2025-02-08 22:21:04 +0800

    初始化

---
+++ src/main/java/com/bitxlab/abacusserver/AbacusServerApplication.java
@@ -0,0 +1,13 @@
+package com.bitxlab.abacusserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacusServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbacusServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitxlab/abacusserver/AbacusServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitxlab.abacusserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacusServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
